55d1959a5dd95ba17471b8fa5a5957fab72c7b28
Katee Dementeva <sancy95@mail.ru>
2019-03-23 05:34:57 +0000

    Add default callback value

--- src/com/katee/Main.kt
+++ src/com/katee/Main.kt
@@ -1,17 +1,19 @@
 package com.katee
 
-import kotlin.system.exitProcess
-
 fun main(args: Array<String>) {
     val status = when (args.size) {
-        0 -> getStatus(0, {println("No Args!!!")})
-            1 -> getStatus(1, {})
-            2 -> getStatus(2, {})
-            else -> getStatus (100, {args.forEach { println(it) }})
+        0 -> getStatus(0) {
+            println("No Args!!!")
+        }
+        1 -> getStatus(1)
+        2 -> getStatus(2)
+        else -> getStatus(100)
+        { args.forEach { println(it) } }
         }
     System.exit(status)
 }
-fun getStatus(status: Int, callback: () -> Unit): Int{
+
+fun getStatus(status: Int, callback: () -> Unit = {}): Int {
     callback()
     return status
 }
