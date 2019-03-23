50bd7e0ea3e12e865da39539091fbce812ba1d42
Katee Dementeva <sancy95@mail.ru>
2019-03-23 03:47:10 +0000

    TEST

--- TEST.sh
+++ TEST.sh
@@ -1,7 +1,34 @@
 ./BUILD.sh
+
 echo
 echo No args
 ./RUN.sh
+RES=$?
+if [ $? -eq 0 ]; then
+    echo Test passed
+else
+    echo est failed. Expected 0 Actual $RES
+    fi
+
+echo
+echo 1 arg
+./RUN.sh foo
+RES=$?
+if [ $? -eq 1 ]; then
+    echo Test passed
+else
+    echo est failed. Expected 1 Actual $RES
+    fi
+
+echo
+echo 2 arg
+./RUN.sh foo bar
+RES=$?
+if [ $? -eq 2 ]; then
+    echo Test passed
+else
+    echo est failed. Expected 2 Actual $RES
+    fi
 
 echo Many args
 ./RUN.sh foo bar baz "1 2 3"
