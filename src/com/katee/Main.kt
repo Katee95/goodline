d70c77301a5569bac43fa001893e880eff715d3c
Katee Dementeva <sancy95@mail.ru>
2019-03-23 04:52:49 +0000

    Add exit codes

--- src/com/katee/Main.kt
+++ src/com/katee/Main.kt
@@ -3,8 +3,14 @@ package com.katee
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("No Args!!!")
-    }else {
+        System.exit(  0)
+    }else if(args.size ==1) {
+        System.exit(1)
+    }else if(args.size ==2) {
+        System.exit(2)
+    } else {
         args.forEach { println(it) }
 
+            System.exit( 100)
     }
 }
